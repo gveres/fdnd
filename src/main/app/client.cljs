54ebdce840d09abf9af41bf1ca1307b1b54840d0
Gábor Veres <gabor.veres@gmail.com>
2020-12-18 10:29:41 +0100

    Plain column and task setup

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -11,13 +11,37 @@
   (action [{:keys [state]}]
           (swap! state update :ui/number inc)))
 
-(defsc Root [this {:ui/keys [number]}]
-  {:query         [:ui/number]
-   :initial-state {:ui/number 0}}
+(defsc Task [this {:task/keys [id content] :as props}]
+  {:query         [:task/id :task/content]
+   :ident         (fn [] [:task/id (:task/id props)])
+   :initial-state (fn [{:keys [id content] :as params}] {:task/id id :task/content content})}
   (dom/div
-    (dom/h4 "This is an example.")
-    (dom/button {:onClick #(comp/transact! this [(bump-number {})])}
-                "You've clicked " number " times")))
+    content))
+
+(def ui-task (comp/factory Task {:keyfn :task/id}))
+
+(defsc Column [this {:column/keys [id title tasks] :as props}]
+  {:query         [:column/id :column/title {:column/tasks (comp/get-query Task)}]
+   :ident         (fn [] [:column/id (:column/id props)])
+   :initial-state (fn [{:keys [id title]}]
+                    {:column/id    id
+                     :column/title title
+                     :column/tasks [(comp/get-initial-state Task {:id 1 :content "Task One"})
+                                    (comp/get-initial-state Task {:id 2 :content "Task Two"})
+                                    (comp/get-initial-state Task {:id 3 :content "Task Three"})
+                                    (comp/get-initial-state Task {:id 4 :content "Task Four"})
+                                    ]})}
+  (dom/div
+    (dom/h4 title)
+    (map ui-task tasks)))
+
+(def ui-column (comp/factory Column {:keyfn :column/id}))
+
+(defsc Root [this {:keys [column]}]
+  {:query         [{:column (comp/get-query Column)}]
+   :initial-state (fn [params] {:column (comp/get-initial-state Column {:id 1 :title "To Do"})})}
+  (dom/div
+    (ui-column column)))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
