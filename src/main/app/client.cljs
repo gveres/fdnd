f02ab87eb6dbe98183b5f378894c339527fb0c40
Gábor Veres <gabor.veres@gmail.com>
2020-12-19 17:28:39 +0100

    Add react-beautiful-dnd to sample

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -4,9 +4,15 @@
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.mutations :refer [defmutation]]
    [com.fulcrologic.fulcro-css.css-injection :as inj]
-   [com.fulcrologic.fulcro-css.localized-dom :as dom]))
+   [com.fulcrologic.fulcro-css.localized-dom :as dom]
+   [com.fulcrologic.fulcro.algorithms.react-interop :as interop :refer [react-factory]]
+   ["react-beautiful-dnd" :refer [DragDropContext Droppable Draggable]]))
 
 (defonce app (app/fulcro-app))
+
+(def ui-drag-drop-context (react-factory DragDropContext))
+(def ui-droppable (react-factory Droppable))
+(def ui-draggable (react-factory Draggable))
 
 (defmutation bump-number [ignored]
   (action [{:keys [state]}]
@@ -16,9 +22,17 @@
   {:query         [:task/id :task/content]
    :ident         (fn [] [:task/id (:task/id props)])
    :initial-state (fn [{:keys [id content] :as params}] {:task/id id :task/content content})
-   :css           [[:.task {:padding "8px"}]]}
-  (dom/div :.task
-           content))
+   :css           [[:.task {:padding "8px"
+                            :border  "1px solid lightgrey"
+                            :margin  "8px"}]]}
+  (ui-draggable {:draggableId (str "task-" id) :index id}
+                (fn [provided]
+                  (let [dprops (merge {:ref (.-innerRef provided)}
+                                      (js->clj (.-dragHandleProps provided))
+                                      (js->clj (.-draggableProps provided)))]
+                    (comp/with-parent-context this
+                      (dom/div :.task dprops
+                               content))))))
 
 (def ui-task (comp/factory Task {:keyfn :task/id}))
 
@@ -32,19 +46,25 @@
                                     (comp/get-initial-state Task {:id 2 :content "Task Two"})
                                     (comp/get-initial-state Task {:id 3 :content "Task Three"})
                                     (comp/get-initial-state Task {:id 4 :content "Task Four"})
-                                    ]})}
-  (dom/div
-    (dom/h4 title)
-    (map ui-task tasks)))
+                                    ]})
+   :css           [[:.title {:padding "8px"}]]}
+  (ui-droppable {:droppableId (str "column-" id)}
+                (fn [provided] (comp/with-parent-context this
+                                 (dom/div {:ref (.-innerRef provided)}
+                                          (dom/h4 :.title title)
+                                          (map ui-task tasks)
+                                          (.-placeholder provided))))))
 
 (def ui-column (comp/factory Column {:keyfn :column/id}))
+
 
 (defsc Root [this {:keys [column]}]
   {:query         [{:column (comp/get-query Column)}]
    :initial-state (fn [params] {:column (comp/get-initial-state Column {:id 1 :title "To Do"})})}
-  (dom/div
-    (inj/style-element {:component Root})
-    (ui-column column)))
+  (ui-drag-drop-context {}
+                        (dom/div
+                          (inj/style-element {:component Root})
+                          (ui-column column))))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
